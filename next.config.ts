df5916330cb3ed5f934eed3edc950ea495d781de
DelValleMateo <tuemail@example.com>
2025-11-26 11:11:54 -0300

    Configurar exportacion estatica para netlify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',  // <--- ESTO ES LA CLAVE: Genera HTML estático
+  images: {
+    unoptimized: true, // <--- NECESARIO: Porque en modo estático Next no puede optimizar imágenes automáticamente
+  },
 };
 
 export default nextConfig;
